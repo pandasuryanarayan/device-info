246eb85ffe296567979ac8ad19c1031ab9872776
licpuranchand <licpuranchand@gmail.com>
2024-07-26 19:56:26 +0000

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,15 @@ import 'package:deviceinfo/device_info.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Get device info
-  await DeviceInfo.getDeviceInfo();
+  final deviceinfo = await DeviceInfo.getDeviceInfo();
+
+  // Print device info
+  print(deviceinfo);
+  print(deviceinfo!['device_id']);
+  print(deviceinfo['device_brand']);
+  print(deviceinfo['device_model']);
+  print(deviceinfo['device_version']);
+  print(deviceinfo['build_id']);
 }
